112267130c81452af7c368bcf535e1bfffb2364c
finnhodgkin <finnhodgkin@gmail.com>
2017-06-23 20:58:57 +0100

    adds hapi server to run at the same time as sockets

--- node-sockets/server.js
+++ node-sockets/server.js
@@ -4,7 +4,7 @@ const server = new hapi.Server();
 
 server.register(require('inert'), function() {});
 
-server.connection({ port: 4000 });
+server.connection({ port: process.env.PORT || 3000 });
 
 server.route({
   method: 'GET',
@@ -18,38 +18,38 @@ server.start(() => {
   console.log('started');
 });
 
-// const ws = new websocket.Server({ port: 5000 });
-//
-// ws.broadcast = data => {
-//   console.log('bc');
-//
-//   ws.clients.forEach(client => {
-//     if (client.readyState === websocket.OPEN) {
-//       client.send(data);
-//     }
-//   });
-// };
-//
-// ws.on('connection', socket => {
-//   console.log(socket);
-//   ws.clients.forEach(client => {
-//     if (client !== socket && client.readyState === websocket.OPEN) {
-//       client.send('Client connection opened');
-//     }
-//   });
-//
-//   socket.on('message', data => {
-//     console.log('data>>', data);
-//     ws.clients.forEach(client => {
-//       if (client !== socket && client.readyState === websocket.OPEN) {
-//         client.send(data);
-//       }
-//     });
-//   });
-//
-//   socket.on('close', () => {
-//     ws.clients.forEach(client => {
-//       client.send('Client connection closed');
-//     });
-//   });
-// });
+const ws = new websocket.Server({ port: process.env.PORT || 3000 });
+
+ws.broadcast = data => {
+  console.log('bc');
+
+  ws.clients.forEach(client => {
+    if (client.readyState === websocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+ws.on('connection', socket => {
+  console.log(socket);
+  ws.clients.forEach(client => {
+    if (client !== socket && client.readyState === websocket.OPEN) {
+      client.send('Client connection opened');
+    }
+  });
+
+  socket.on('message', data => {
+    console.log('data>>', data);
+    ws.clients.forEach(client => {
+      if (client !== socket && client.readyState === websocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+
+  socket.on('close', () => {
+    ws.clients.forEach(client => {
+      client.send('Client connection closed');
+    });
+  });
+});
